a3e73687721449b1607f41cf70f2ded88984dac3
ccharekian <ccharekian@SQ241-07>
2017-02-22 18:13:55 -0800

    woa

--- itgs_ia_v2/itgs_ia_v2/sdk.h
+++ itgs_ia_v2/itgs_ia_v2/sdk.h
@@ -8,7 +8,10 @@
 #include <stdio.h>
 
 #include "avgpace.h"
-
+#pragma comment( lib, "glfw3.lib" )
 #include "imgui.h"
 #include "imgui_internal.h"
 #include "imgui_impl_glfw_gl3.h"
+#include "libs\gl3w\GL\gl3w.h"
+#include "libs\glfw\include\GLFW\glfw3.h"
+#include "libs\glfw\include\GLFW\glfw3native.h""
